eea2af6475219ccd0fc672f028bc97df09133880
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-12-22 23:35:34 -0500

    Update get-routes.js

--- routes/get-routes.js
+++ routes/get-routes.js
@@ -2,29 +2,55 @@ const profile = require("../services/profile");
 const passport = require("passport");
 
 module.exports = (app) => {
-  app.get(
+  // app.get(
+  //   "/api/profile",
+  //   passport.authenticate("jwt", { session: false }),
+  //   async (req, res) => {
+  //     try {
+  //       const existingProfile = await profile.getProfileInfo(req.user.userId);
+  //         console.log("here in existing new profile")
+  //       if (existingProfile) {
+  //         res.json(existingProfile);
+  //       } else {
+  //         console.log("here in creating new profile")
+  //         const result = await profile.registerProfile(req.user.userId);
+  //         console.log("result ",result)
+  //         if (result) {
+  //           res.json({
+  //             profile: result,
+  //             message: "Registered new profile for a user" + req.user.userId,
+  //           });
+  //         }
+  //       }
+  //     } catch (e) {
+  //       res.status(422).json({ message: e });
+  //     }
+  //   }
+  // );
+
+   app.get(
     "/api/profile",
     passport.authenticate("jwt", { session: false }),
-    async (req, res) => {
-      try {
-        const existingProfile = await profile.getProfileInfo(req.user.userId);
-          console.log("here in existing new profile")
-        if (existingProfile) {
-          res.json(existingProfile);
-        } else {
-          console.log("here in creating new profile")
-          const result = await profile.registerProfile(req.user.userId);
-          console.log("result ",result)
-          if (result) {
-            res.json({
-              profile: result,
-              message: "Registered new profile for a user" + req.user.userId,
+    (req, res) => {
+      profile
+        .getProfileInfo(req.user.userId)
+        .then((profile) => res.json({ profile: profile, message: "Success" }))
+        .catch((err) => {
+          console.log(err);
+          profile
+            .registerProfile(req.user.userId)
+            .then((profile) => {
+              console.log("Got to this point");
+              res.json({
+                profile: profile,
+                message: "Registered new profile for a user" + req.user.userId,
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(422).json({ message: err });
             });
-          }
-        }
-      } catch (e) {
-        res.status(422).json({ message: e });
-      }
+        });
     }
   );
 };
