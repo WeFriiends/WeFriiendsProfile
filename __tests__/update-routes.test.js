8ce4889f0c997857c1068491aed3ef27a637b7d2
Aleksandra <abaibik@gmail.com>
2024-02-13 15:37:02 +0100

    feat: update tests

--- __tests__/update-routes.test.js
+++ __tests__/update-routes.test.js
@@ -40,6 +40,91 @@ describe("UPDATE /api/profiles/:id", () => {
       .set("Authorization", `Bearer ${token}`);
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.name).toBe("Jane Dow");
+    expect(response.body).toEqual(_updatedProfile);
+  });
+
+  it("returns 401 if trying to update someone else's profile", async () => {
+    const userId = "test-user-id";
+    const anotherId = "someone-else's-id";
+
+    const changes = {
+      name: "Jane Dow",
+    };
+
+    const _updatedProfile = {
+      _id: "65bf74e4bbc75c11e6e83ce0",
+      name: "Jane Dow",
+      dob: "1998-12-31T23:00:00.000Z",
+      zodiacSign: "",
+      gender: "F",
+      reason: [],
+      location: {},
+      bio: "",
+      photos: [],
+    };
+
+    mockingoose(model).toReturn(_updatedProfile, "findOneAndUpdate");
+
+    const someoneElsesPayload = {
+      _id: "789",
+      anotherId,
+    };
+
+    const token = jwt.sign(someoneElsesPayload, jwtSecret);
+
+    const response = await request(app)
+      .patch(`/api/profiles/${userId}`)
+      .send(changes)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(401);
+  });
+
+  it("doesn't update profile if not authed", async () => {
+    const userId = "test-user-id";
+    const changes = {
+      name: "Jane Dow",
+    };
+
+    const response = await request(app)
+      .patch(`/api/profiles/${userId}`)
+      .send(changes);
+
+    expect(response.statusCode).toBe(401);
+  });
+
+  it("returns 422 when trying to update non-existent property", async () => {
+    const userId = "test-user-id";
+
+    const changes = {
+      nonExistentKey: "updatedValue",
+    };
+
+    const _existingProfile = {
+      _id: "65bf74e4bbc75c11e6e83ce0",
+      name: "Jane Dow",
+      dob: "1998-12-31T23:00:00.000Z",
+      zodiacSign: "",
+      gender: "F",
+      reason: [],
+      location: {},
+      bio: "",
+      photos: [],
+    };
+
+    mockingoose(model).toReturn(_existingProfile, "findOneAndUpdate");
+
+    const payload = {
+      _id: "123",
+      userId,
+    };
+    const token = jwt.sign(payload, jwtSecret);
+
+    const response = await request(app)
+      .patch(`/api/profiles/${userId}`)
+      .send(changes)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(422);
   });
 });
