92c0d8ad3992077ac57e7e51f6657d169c32836b
ViktoriaSharpershape <viktoriia.shevchenko@sharpershape.com>
2024-03-29 11:50:17 +0200

    fixed the upload function

--- routes/photo-routes.js
+++ routes/photo-routes.js
@@ -22,14 +22,14 @@ module.exports = (app) => {
     upload.single("file"),
     async (req, res) => {
       try {
-        const cloudinaryUpload = await cloudinary.uploader.upload(req.file.buffer, { folder: "profile" });
+        const uploadResult = await cloudinary.uploader.upload(req.file.buffer, { folder: "profile" });
 
-        await photoService.addPhoto(req.user.userId, cloudinaryUpload.url);
+        await photoService.addPhoto(req.user.userId, uploadResult.url);
 
-        return res.status(200).send({ message: "Photo uploaded successfully", cloudinary: cloudinaryUpload });
+        return res.status(200).send({ message: "Photo uploaded successfully", cloudinary: uploadResult });
       } catch (error) {
         console.error(error);
-        return res.status(500).send({ err: "Error uploading photo" });
+        return res.status(500).send({ error: "Error uploading photo" });
       }
     }
   );
