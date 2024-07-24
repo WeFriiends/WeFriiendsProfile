f1cadf6f8967912bd199ffd4afcde920be83933a
Lyubov Sugak <L.Sugak84@gmail.com>
2024-07-24 23:28:23 +0200

    move all neccessary data from prev repo

---
+++ src/middleware/uploadPhoto.ts
@@ -0,0 +1,85 @@
+import dotenv from "dotenv";
+import multer, { Multer } from "multer";
+import {
+  v2 as cloudinary,
+  UploadApiResponse,
+  UploadApiErrorResponse,
+  UploadApiOptions,
+} from "cloudinary";
+import sharp from "sharp";
+import { Request, NextFunction } from "express";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+interface CloudinaryFile extends Express.Multer.File {
+  buffer: Buffer;
+}
+
+interface CustomRequest extends Request {
+  files?: CloudinaryFile[];
+  body: {
+    cloudinaryUrls?: string[];
+  };
+}
+
+const storage = multer.memoryStorage();
+export const upload: Multer = multer({ storage: storage });
+
+export const uploadToCloudinary = async (
+  req: CustomRequest,
+  next: NextFunction
+) => {
+  try {
+    const files: CloudinaryFile[] = req.files as CloudinaryFile[];
+    if (!files || files.length === 0) {
+      return next(new Error("No files provided"));
+    }
+    const cloudinaryUrls: string[] = [];
+    let processedFiles = 0;
+
+    for (const file of files) {
+      const resizedBuffer: Buffer = await sharp(file.buffer)
+        .resize({ width: 800, height: 600 })
+        .toBuffer();
+
+      const options: UploadApiOptions = {
+        resource_type: "auto",
+        folder: "profile-photos",
+      };
+
+      const uploadStream = cloudinary.uploader.upload_stream(
+        options,
+        (
+          err: UploadApiErrorResponse | undefined,
+          result: UploadApiResponse | undefined
+        ) => {
+          if (err) {
+            console.error("Cloudinary upload error:", err);
+            return next(err);
+          }
+          if (!result) {
+            console.error("Cloudinary upload error: Result is undefined");
+            return next(new Error("Cloudinary upload result is undefined"));
+          }
+          cloudinaryUrls.push(result.secure_url);
+          processedFiles++;
+
+          if (processedFiles === files.length) {
+            req.body.cloudinaryUrls = cloudinaryUrls;
+            next();
+          }
+        }
+      );
+      uploadStream.end(resizedBuffer);
+    }
+  } catch (error) {
+    console.error("Error in uploadToCloudinary middleware:", error);
+    next(error);
+  }
+};
---
+++ src/routes/photo.ts
@@ -0,0 +1,109 @@
+import { Router } from "express";
+import { upload, uploadToCloudinary } from "../middleware/uploadPhoto";
+import { handleUploadToCloudinary } from "../controllers/photo";
+import * as photoController from "../controllers/photo";
+
+const router = Router();
+
+/**
+ * @swagger
+ * /api/photos:
+ *   post:
+ *     summary: Upload images to Cloudinary
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - in: formData
+ *         name: images
+ *         type: array
+ *         items:
+ *           type: string
+ *           format: binary
+ *         description: Array of images to upload
+ *     responses:
+ *       200:
+ *         description: Successfully uploaded images
+ *       500:
+ *         description: Internal server error
+ */
+router.post(
+  "/",
+  upload.array("images", 5),
+  // @ts-ignore
+  uploadToCloudinary,
+  handleUploadToCloudinary
+);
+
+/**
+ * @swagger
+ * /api/photos:/{id}:
+ *   get:
+ *     summary: Get profile photos by user ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User ID
+ *     responses:
+ *       200:
+ *         description: List of profile photos
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/:id", photoController.getPhotos);
+
+/**
+ * @swagger
+ * /api/photos:
+ *   post:
+ *     summary: Add a new photo URL to a user's profile
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *                 description: User ID
+ *               photoUrl:
+ *                 type: string
+ *                 description: Photo URL to add
+ *     responses:
+ *       200:
+ *         description: Successfully added photo
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/", photoController.addPhoto);
+
+/**
+ * @swagger
+ * /api/photos:
+ *   delete:
+ *     summary: Remove a photo URL from a user's profile
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *                 description: User ID
+ *               photoId:
+ *                 type: string
+ *                 description: Photo ID to remove
+ *     responses:
+ *       200:
+ *         description: Successfully removed photo
+ *       500:
+ *         description: Internal server error
+ */
+router.delete("/", photoController.removePhoto);
+
+export default router;
