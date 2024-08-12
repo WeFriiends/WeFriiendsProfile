7ef7835749e14957500742efb8c508aaf355535a
ViktoriaSharpershape <viktoriia.shevchenko@sharpershape.com>
2024-08-12 11:12:30 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 import profileRoutes from "./routes/profile";
 import photosRoutes from "./routes/photo";
+import usersRoutes from './routes/user'
+import chatsRoutes from './routes/chat'
 
 dotenv.config();
 
@@ -64,6 +66,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Routes
 app.use("/api/profile", profileRoutes);
 app.use("/api/photos", photosRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/chats", chatsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
