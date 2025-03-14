9f5ec599eb69d39338d04970d399d238d7a08c86
Marina Kotelnikov <oce4ka@gmail.com>
2025-03-14 17:43:09 +0200

    fix: new profile, change profile, get profile have been fixed

--- src/models/profileModel.ts
+++ src/models/profileModel.ts
@@ -9,6 +9,17 @@ export interface Location {
   houseNumber?: string;
 }
 
+export interface Preferences {
+  aboutMe?: string;
+  selectedLanguages?: string[];
+  Smoking?: string[];
+  EducationalLevel?: string[];
+  Children?: string[];
+  Drinking?: string[];
+  Pets?: string[];
+  Interests?: string[];
+}
+
 export interface ProfileDocument extends Document {
   _id: string;
   name: string;
@@ -20,6 +31,11 @@ export interface ProfileDocument extends Document {
   photos?: string[];
   gender: string;
   reasons: string[];
+  preferences?: Preferences;
+  friendsAgeMin?: number;
+  friendsAgeMax?: number;
+  friendsDistance?: number;
+  profileCreated: Date;
 }
 
 const profileSchema = new Schema<ProfileDocument>({
@@ -38,6 +54,20 @@ const profileSchema = new Schema<ProfileDocument>({
   photos: { type: [String], default: [] },
   gender: { type: String },
   reasons: { type: [String], default: [] },
+  preferences: {
+    aboutMe: { type: String },
+    selectedLanguages: { type: [String], default: [] },
+    Smoking: { type: [String], default: [] },
+    EducationalLevel: { type: [String], default: [] },
+    Children: { type: [String], default: [] },
+    Drinking: { type: [String], default: [] },
+    Pets: { type: [String], default: [] },
+    Interests: { type: [String], default: [] },
+  },
+  friendsAgeMin: { type: Number, default: 18 },
+  friendsAgeMax: { type: Number },
+  friendsDistance: { type: Number, default: 50 },
+  profileCreated: { type: Date, default: () => new Date() },
 });
 
 const Profile = mongoose.model<ProfileDocument>("Profile", profileSchema);
--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { upload } from "../middleware/upload";
 import { checkJwt } from "../middleware/checkJwt";
 import {
   registerProfile,
@@ -7,7 +8,6 @@ import {
   deleteProfile,
   getAllProfiles,
 } from "../controllers/profile";
-import { getAllUsers } from "../controllers/user";
 
 const router = Router();
 
@@ -62,7 +62,8 @@ const router = Router();
  *       400:
  *         description: Bad request
  */
-router.post("/", checkJwt, registerProfile);
+//router.post("/", checkJwt, registerProfile);
+router.post("/", upload.any(), registerProfile);
 
 /**
  * @swagger
@@ -86,6 +87,7 @@ router.post("/", checkJwt, registerProfile);
  *                   type: string
  */
 router.get("/", checkJwt, getCurrentProfile);
+//router.get("/",getCurrentProfile);
 
 /**
  * @swagger
