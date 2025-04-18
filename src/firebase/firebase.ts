67011d222dd40c54069f06d94969e12407093b84
Dmitry <demosne@gmail.com>
2025-04-18 15:38:24 +0300

    Add Firebase Admin SDK integration and CRUD endpoints for Firestore
    - Updated .gitignore to exclude firebaseAdminsdk.json.
    - Added firebase-admin dependency to package.json and package-lock.json.
    - Created firebase.ts to initialize Firebase Admin SDK.
    - Implemented CRUD operations for Firestore in index.ts, including endpoints for creating, reading, and deleting documents.
    - Added error handling for Firestore operations.
    - Updated Swagger documentation to include new Firebase endpoints.

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,16 @@
+import admin from "firebase-admin";
+
+try {
+  const serviceAccount = require("./firebaseAdminsdk.json");
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    // databaseURL: 'your_database_url'
+  });
+
+  console.log("Firebase Admin SDK initialized successfully.");
+} catch (error) {
+  console.error("Error initializing Firebase Admin SDK:", error);
+}
+
+export default admin;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
+import admin from "./firebase/firebase";
 import dotenv from "dotenv";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 import profileRoutes from "./routes/profile";
 import photosRoutes from "./routes/photo";
-import chatsRoutes from './routes/chat'
+import chatsRoutes from "./routes/chat";
 
 dotenv.config();
 
 const app = express();
+const db = admin.firestore();
 const PORT = process.env.PORT || 8080;
 
 // Allow requests from your frontend's origin
@@ -63,6 +65,71 @@ const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
+app.get("/firebase", async (req, res) => {
+  try {
+    const snapshot = await db.collection(process.env.FIREBASE_MATCHES!).get();
+    const data: any[] = [];
+    snapshot.forEach((doc) => {
+      data.push({ id: doc.id, ...doc.data() });
+    });
+    res.json(data);
+  } catch (error) {
+    console.error("Error getting data:", error);
+    res.status(500).send("Error fetching data");
+  }
+});
+
+app.delete("/firebase", async (req, res) => {
+  try {
+    const snapshot = await db.collection(process.env.FIREBASE_MATCHES!).get();
+
+    if (snapshot.empty) {
+      return res.json({ message: "No documents to delete" });
+    }
+
+    const deletePromises = snapshot.docs.map((doc) =>
+      db.collection(process.env.FIREBASE_MATCHES!).doc(doc.id).delete()
+    );
+
+    await Promise.all(deletePromises);
+    res.json({ message: "All documents deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting data:", error);
+    res.status(500).json({ message: "Error deleting documents" });
+  }
+});
+
+app.get("/firebase/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const elem = await db
+      .collection(process.env.FIREBASE_MATCHES!)
+      .doc(id)
+      .get();
+    if (!elem.exists) {
+      res.status(404).send("Document not found");
+      return;
+    }
+    res.json({ id: elem.id, ...elem.data() });
+  } catch (error) {
+    console.error("Error getting data:", error);
+    res.status(500).send("Error fetching data");
+  }
+});
+
+app.post("/firebase", async (req, res) => {
+  try {
+    const data = req.body;
+
+    const result = await db.collection(process.env.FIREBASE_MATCHES!).add(data);
+
+    res.json(result);
+  } catch (error) {
+    console.error("Error getting data:", error);
+    res.status(500).send("Error fetching data");
+  }
+});
+
 app.use("/api/profile", profileRoutes);
 app.use("/api/photos", photosRoutes);
 app.use("/api/chats", chatsRoutes);
