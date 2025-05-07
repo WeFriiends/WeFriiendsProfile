af547f0172baced0ace73c6b1ba00d6d3972f525
demos-prog <73884833+demos-prog@users.noreply.github.com>
2025-05-07 09:37:55 +0300

    Add match functionality to the API (#30)
    * Add match functionality to the API
    
    - Introduced MatchController to handle match-related operations including adding, retrieving, and removing matches.
    - Created MatchService to manage match data interactions with the database.
    - Defined Match model with user1_id and user2_id fields.
    - Implemented match routes for adding, retrieving, and deleting matches, secured with JWT authentication.
    - Updated Swagger documentation for the new match endpoints.
    
    * Enhance match controller with user existence check
    
    - Added a check to verify if user2_id exists in the profile model before proceeding with match creation.
    - Updated error handling to return a 404 status if user2_id does not exist.
    - Refactored log message in the removeMatch method for clarity.
    
    * Refactor error message in match controller for user existence check
    
    - Updated the error message returned when user2_id does not exist to improve clarity and consistency.

---
+++ src/controllers/match.controller.ts
@@ -0,0 +1,93 @@
+import { Request, Response } from "express";
+import { MatchService } from "../services/match.service";
+import { extractUserId } from "../utils/auth";
+import Profile from "../models/profileModel";
+
+export class MatchController {
+  private matchService: MatchService;
+
+  constructor(matchService: MatchService) {
+    this.matchService = matchService;
+  }
+
+  addMatch = async (req: Request, res: Response) => {
+    console.log("controller addMatch");
+    const userId = extractUserId(req);
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
+    }
+    try {
+      const user2_id = req.body.user2_id;
+      if (!user2_id) {
+        return res.status(400).json({ message: "user2_id is required" });
+      }
+
+      const isUser2Exist = await Profile.findById(user2_id);
+      if (!isUser2Exist) {
+        return res
+          .status(404)
+          .json({ message: "User with this id doesn't exist" });
+      }
+
+      if (userId === user2_id) {
+        return res
+          .status(400)
+          .json({ message: "Can not add yourself as a match" });
+      }
+
+      const newMatch = await this.matchService.addMatch(userId, user2_id);
+      return res.status(200).json(newMatch);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      }
+      return res.status(500).json({ message: "An unknown error occurred" });
+    }
+  };
+
+  getMatches = async (req: Request, res: Response) => {
+    console.log("controller getMatches");
+    const userId = extractUserId(req);
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
+    }
+    try {
+      const matches = await this.matchService.getMatches(userId);
+      return res.status(200).json(matches);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(404).json({ message: error.message });
+      }
+      return res.status(500).json({ message: "An unknown error occurred" });
+    }
+  };
+
+  removeMatch = async (req: Request, res: Response) => {
+    console.log("controller removeMatch");
+    const userId = extractUserId(req);
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
+    }
+    try {
+      const user2_id = req.body.user2_id;
+
+      if (!user2_id) {
+        return res.status(400).json({ message: "user2_id is required" });
+      }
+
+      const result = await this.matchService.removeMatch(userId, user2_id);
+      return res.status(200).json(result);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(404).json({ message: error.message });
+      }
+      return res.status(500).json({ message: "An unknown error occurred" });
+    }
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import cors from "cors";
 import profileRoutes from "./routes/profile";
 import photosRoutes from "./routes/photo";
 import chatsRoutes from "./routes/chat";
+import matchRoutes from "./routes/match.route";
 import likesRoutes from "./routes/likes.route";
 
 dotenv.config();
@@ -65,6 +66,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.use("/api/profile", profileRoutes);
+app.use("/api/matches", matchRoutes);
 app.use("/api/likes", likesRoutes);
 app.use("/api/photos", photosRoutes);
 app.use("/api/chats", chatsRoutes);
