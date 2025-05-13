fb9e387939970a524c6b25fea90cfcb4c0571919
Dmitry <demosne@gmail.com>
2025-05-13 09:07:51 +0300

    Refactor model imports and add new schemas for chat, likes, dislikes, and profile
    - Updated index.ts to import models from their respective module directories for better organization.
    - Introduced new Mongoose schemas for Chat, Likes, Dislikes, and Profile, enhancing the data structure and functionality of the application.
    - Added friendSearchProjection to streamline friend search queries.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,11 @@
-export { default as Chat } from "./chat.model";
-export { default as Dislikes } from "./dislikes.model";
-export { default as friendSearchProjection } from "./friendSearchProjection";
-export { default as Likes } from "./likes.model";
-export { default as Match } from "./match.model";
-export { default as Profile } from "./profile.model";
-export { Location, Preferences, ProfileDocument } from "./profile.model";
+export { default as Chat } from "../modules/chat/chat.model";
+export { default as Dislikes } from "../modules/deslikes/dislikes.model";
+export { default as friendSearchProjection } from "../modules/profile/friendSearchProjection";
+export { default as Likes } from "../modules/likes/likes.model";
+export { default as Match } from "../modules/match/match.model";
+export { default as Profile } from "../modules/profile/profile.model";
+export {
+  Location,
+  Preferences,
+  ProfileDocument,
+} from "../modules/profile/profile.model";
