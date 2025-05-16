feb99915df88d0944229cfc2c7e8ed119347f02f
Dmitry <demosne@gmail.com>
2025-05-16 12:02:29 +0300

    - Removed unnecessary comments

--- src/config/webpush.ts
+++ src/config/webpush.ts
@@ -3,16 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// In production, use environment variables for VAPID keys
 const publicKey = process.env.VAPID_PUBLIC_KEY;
 const privateKey = process.env.VAPID_PRIVATE_KEY;
 
-// Generate keys only if they don't exist in environment
 const vapidKeys = publicKey && privateKey 
   ? { publicKey, privateKey } 
   : webPush.generateVAPIDKeys();
 
-// If keys were generated, log them so they can be added to environment variables
 if (!publicKey || !privateKey) {
   console.log("Generated VAPID Keys - add these to your environment variables:");
   console.log(vapidKeys);
