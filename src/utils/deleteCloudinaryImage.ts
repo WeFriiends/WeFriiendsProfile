8cc40cee6e23a2b1d6f4289b7f2fa7d2e25d64db
Dmitry <demosne@gmail.com>
2025-06-09 11:48:14 +0300

    Refactor Cloudinary image deletion utility

--- src/utils/deleteCloudinaryImage.ts
+++ src/utils/deleteCloudinaryImage.ts
@@ -1,10 +1,4 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
-});
+import cloudinary from "../config/cloudinary";
 
 export async function deleteCloudinaryImage(photoId: string) {
   if (!photoId) {
