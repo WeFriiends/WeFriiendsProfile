d85aa3e3ee9185d3bf16e8311c890cc5b039a4da
Dmitry <demosne@gmail.com>
2026-01-16 11:49:26 +0300

    Enhance Like model with timestamps
    - Updated LikedUserSchema and likeSchema to include timestamps for better tracking of liked events.
    - Improved schema structure for clarity and maintainability.

--- src/modules/like/like.model.ts
+++ src/modules/like/like.model.ts
@@ -1,14 +1,20 @@
 import mongoose, { Schema } from "mongoose";
 
-const LikedUserSchema = new Schema({
-  liked_id: { type: String, required: true },
-  liked_at: { type: Date, required: true },
-});
+const LikedUserSchema = new Schema(
+  {
+    liked_id: { type: String, required: true },
+    liked_at: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
 
-const likeSchema = new Schema({
-  liker_id: { type: String, required: true },
-  likes: { type: [LikedUserSchema], required: true },
-});
+const likeSchema = new Schema(
+  {
+    liker_id: { type: String, required: true },
+    likes: { type: [LikedUserSchema], required: true },
+  },
+  { timestamps: true }
+);
 
 const Like = mongoose.model("Like", likeSchema);
 
