1f5f09563f29700188d0ad9dc671345d45c387b5
Evgenii <evgeniiproger@gmail.com>
2026-04-16 08:47:33 +0500

    fix: move databaseURL to environment variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,7 +7,7 @@ if (!admin.apps.length) {
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
             privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         }),
-        databaseURL: 'https://wefriiends-f8f52-default-rtdb.europe-west1.firebasedatabase.app'
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
     })
 }
 
